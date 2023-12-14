ba56e517519912c7d808a81bb9787e2622ee1ab9
theharism <85572852+theharism@users.noreply.github.com>
2023-12-14 21:53:15 +0500

    update: Testimonial

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import "../style/testimonials.css";
+import { Rating } from "@mui/material";
 
 const TestimonialComponent = ({ item }) => {
+  console.log(item);
+
+  const imageURI = `${process.env.REACT_APP_BASE_URL}/api/images/${item.filePath}`;
+  console.log(imageURI);
   return (
     <div className="testiContainer">
-      <h5>
-        {item.name},{" "}
-        <span style={{ color: "#150261" }}>{item.designation}</span>
-      </h5>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img src={imageURI} alt="img" className="rounded-image" />
 
+        <h6>
+          {item.name},{" "}
+          <span style={{ color: "#150261" }}>{item.designation}</span>
+        </h6>
+      </div>
       <p>"{item.review}"</p>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 10,
+          left: 75,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Rating name="read-only" value={5} readOnly size="large" />
+      </div>
     </div>
   );
 };
