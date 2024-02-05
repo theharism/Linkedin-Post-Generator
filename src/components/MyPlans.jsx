6491bb1d80a42171bb95258cc059061ab9afeb55
theharism <85572852+theharism@users.noreply.github.com>
2024-02-05 19:49:32 +0500

    fix: my plans

--- src/components/MyPlans.jsx
+++ src/components/MyPlans.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import "../style/MyPlans.css";
-import {
-  MonthlyPro,
-  MonthlyStarter,
-  UpgradePlan,
-  YearlyPro,
-  YearlyStarter,
-} from "./Package";
+import { CustomizePlan } from "./Package";
 import Swal from "sweetalert2";
 import { resetSubscription } from "../slices/SubscriptionSlice";
+import Modal from "./Modal";
+import { Row } from "react-bootstrap";
 
 const MyPlans = ({ type, onClose }) => {
-  const email = useSelector((state) => state.User.email);
-  const { currentUserId } = useSelector((state) => state.Auth);
+  const { email } = useSelector((state) => state.User);
+  const subscription = useSelector((state) => state.Subscription);
   const dispatch = useDispatch();
   const handleUpgrade = () => {
     onClose();
@@ -27,13 +22,7 @@ const MyPlans = ({ type, onClose }) => {
     }
   };
 
-  const handleOverlayClick = (e) => {
-    if (e.target === e.currentTarget) {
-      onClose(); // Close the modal when clicking on the overlay
-    }
-  };
-
-  const handlePayment = async () => {
+  const handleCancelPlan = async () => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -47,68 +36,50 @@ const MyPlans = ({ type, onClose }) => {
         try {
           const response = await axios.post(
             `${process.env.REACT_APP_BASE_URL}/api/cancelSubscription`,
-            { email: currentUserId }
+            { email }
           );
 
           if (response.data.message) {
             Swal.fire({
               title: response.data.message,
               icon: "success",
-              showConfirmButton: false, // Hide the "OK" button in the success popup
-              timer: 1000,
+              showConfirmButton: true,
             });
             dispatch(
               resetSubscription({
-                subscription: {
-                  id: null,
-                  createdDate: "",
-                  expiresDate: "",
-                  type: "Free",
-                },
+                ...subscription,
+                id: "***",
               })
             );
           }
         } catch (error) {
           console.log(error);
           Swal.fire({
-            title: error.response.data.error,
-            icon: "warning",
+            title: error.response.data.message,
+            icon: "error",
             showConfirmButton: false, // Hide the "OK" button in the success popup
-            timer: 1000,
+            timer: 1500,
           });
         }
       }
     });
   };
 
-  const ShowCurrentSubscription = () => {
-    switch (type) {
-      case "Starter (Monthly)":
-        return <MonthlyStarter handlePayment={handlePayment} cancel={true} />;
-      case "Pro (Monthly)":
-        return <MonthlyPro handlePayment={handlePayment} cancel={true} />;
-      case "Starter (Yearly)":
-        return <YearlyStarter handlePayment={handlePayment} cancel={true} />;
-      case "Pro (Yearly)":
-        return <YearlyPro handlePayment={handlePayment} cancel={true} />;
-      default:
-        break;
-    }
-  };
-
   return (
-    <div className="modal" onClick={handleOverlayClick}>
-      <div className="modal-content">
-        <div className="containerForm ">
-          <Col className="row d-flex flex-row justify-content-evenly w-100">
-            <div style={{ height: "50px" }} />
-            <UpgradePlan handleUpgrade={handleUpgrade} />
-            <ShowCurrentSubscription />
-            <div style={{ height: "50px" }} />
-          </Col>
-        </div>
-      </div>
-    </div>
+    <Modal
+      onClose={onClose}
+      heading={"My Plans"}
+      subheading={"Customize your plans according to your needs"}
+    >
+      <Row className="justify-content-evenly">
+        <CustomizePlan title={"Upgrade"} type={1} onClick={handleUpgrade} />
+        <CustomizePlan
+          title={type.split(" ")[0]}
+          type={2}
+          onClick={handleCancelPlan}
+        />
+      </Row>
+    </Modal>
   );
 };
 
